5cb11b3127afae627adbba1f922e6490ab6691b0
Dhakshayani Padala <dhakshayanip@infinite.com>
2022-09-27 15:54:28 +0530

    assignment

---
+++ c#sharp/day1.net/program2/program2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Input an number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n >= 0)
+                Console.WriteLine("{0} is a positive number.\n", n);
+            else
+                Console.WriteLine("{0} ia a negative number.\n", n);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ c#sharp/day1.net/program3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            char operation;
+            Console.Write("Input first number:");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input operation:");
+            operation=Convert.ToChar(Console.ReadLine());
+            Console.Write("Input second number:");
+            b=Convert.ToInt32(Console.ReadLine());
+            if(operation=='+')
+                Console.WriteLine("{0}+{1}={2}",a,b,a+b);
+            else if(operation=='-')
+                Console.WriteLine("{0}-{1}={2}",a,b,a-b);
+            else if(operation=='*')
+                Console.WriteLine("{0}*{1}={2}",a,b,a*b);
+            else if(operation=='/')
+                Console.WriteLine("{0}/{1}={2}",a,b,a/b);
+            else
+                Console.WriteLine("invalid character");
+            Console.ReadLine();
+        }
+    }
+}
